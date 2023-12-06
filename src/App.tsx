d0357854a6c965aad66f1f41af05ed1e3153fe56
T0nete <tonbarramon@gmail.com>
2023-12-06 19:17:53 +0100

    Add Dockerfile and docker-compose.yml for containerization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/api/languages')
+      console.log(response)
+    }
+
+    fetchData()
+  })
 
   return (
     <>
@@ -18,16 +28,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
